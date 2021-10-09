6101b1b835a9202b6131b88a644c440902056c7a
Keye Li <kli32@stevens.edu>
2021-10-09 18:52:41 -0400

    mock practice

--- GetRepo.py
+++ GetRepo.py
@@ -2,11 +2,78 @@ import requests
 import unittest
 import json
 
+from unittest import mock
+from unittest.mock import Mock
 
 
-def getRepoAndCommit (username):
 
 
+# def getRepoAndCommit (username):
+
+
+#     if (not isinstance(username,str)):
+        
+#         return "username should be a string."
+
+    
+#     if(len(username.strip()) == 0):
+        
+#         return "username cannot be just empty spaces."
+
+
+#     response  = requests.get('https://api.github.com/users/' + username +'/repos')
+
+    
+    
+   
+#     if(response.status_code != 200):
+        
+
+#         return "Cannot access data."
+
+
+#     repos = json.loads(response.text)
+
+#     repoNames = []
+
+#     result = []
+
+
+#     for repo in repos:
+        
+#         # name = repo['name']
+
+#         # repoNames.append(name)
+
+#         # response2 = requests.get('https://api.github.com/repos/' + username + '/' + repo['name'] +'/commits')
+
+#         # commits = json.loads(response2.text)
+
+#         # single = "Repo: " + name + " Number of commits: " + str(len(commits))
+        
+#         # # print(single)
+
+#         # result.append(single)
+#         result.append(repo['name'])
+        
+
+
+#     if(len(repoNames) == 0):
+       
+#         return "this user has no repos."
+
+
+      
+
+#     return result
+
+
+'''
+Since my original function calls requests.get() multiple times, and mock can only return 1 fixed data for requests.get(), 
+I have to rewrite my funciton into 2 different functions, one for get repo and one for get commit.
+'''
+
+def getRepo(username):
     if (not isinstance(username,str)):
         
         return "username should be a string."
@@ -30,63 +97,84 @@ def getRepoAndCommit (username):
 
     repos = json.loads(response.text)
 
-    repoNames = []
+    # repos = response.json()
+
+  
 
     result = []
 
-
     for repo in repos:
-        
-        name = repo['name']
-
-        repoNames.append(name)
-
-        response2 = requests.get('https://api.github.com/repos/' + username + '/' + repo['name'] +'/commits')
-
-        commits = json.loads(response2.text)
-
-        single = "Repo: " + name + " Number of commits: " + str(len(commits))
-        
-        # print(single)
-
-        result.append(single)
-
-
-    if(len(repoNames) == 0):
-       
-        return "this user has no repos."
-
-        
+        result.append(repo.get('name'))
 
     return result
 
 
 
-# print(getRepoAndCommit("nebulouspianist"))
+def getCommit(username, repo):
+    if (not isinstance(username,str)):
+        
+        return "username should be a string."
 
-
-class Test (unittest.TestCase):
-
-    def test1(self):
-
-        # since it will be too messy to test the whole list, I will just test the length of the list.
-        self.assertEqual(len(getRepoAndCommit("nebulouspianist")),19)
-
- 
     
-    def test2(self):
-        self.assertEqual(getRepoAndCommit('   '), "username cannot be just empty spaces.")
+    if(len(username.strip()) == 0):
+        
+        return "username cannot be just empty spaces."
+
+    
+    if (not isinstance(repo,str)):
+        
+        return "username should be a string."
+
+    
+    if(len(repo.strip()) == 0):
+        
+        return "username cannot be just empty spaces."
+
+    response  = requests.get('https://api.github.com/users/' + username +'/' + repo +'/commits')
+    commits = json.loads(response.text)
+
+    return len(commits)
+
+
+
+class APITest (unittest.TestCase):
+
+
+    @mock.patch('requests.get')
+
+    def test1(self, mockedReq):
+
+        mockedReq.return_value = Mock(status_code = 200, text = '[{"name":"Complexity"}, {"name": "repo2"}]')
+        
+
+        self.assertEqual(len(getRepo("nebulouspianist")),2)
+        self.assertEqual(getRepo("nebulouspianist"), ['Complexity','repo2'])
+
+    
+
+
+
+    @mock.patch('requests.get')
+    
+    def test2(self, mockedReq):
+        mockedReq.return_value = Mock(status_code = 200, text = '[{"commit": "commit1"}, {"commit": "commit2"},{"commit": "commit3"}]')
+        self.assertEqual(getCommit("nebulouspianist","Complexity"), 3)
+
+
+
+
     
     def test3(self):
-        self.assertEqual(getRepoAndCommit(123), "username should be a string.")
+        self.assertEqual(getRepo(123), "username should be a string.")
 
     
+
 
 if __name__ == "__main__":
     unittest.main()
     
 
-    
+
 
 
 
